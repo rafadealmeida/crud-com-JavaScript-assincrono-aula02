1301ffc9443c00ccbcc8e981f40e7f180412da16
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-03-31 16:49:25 -0300

    Initial commit

---
+++ aula2/admin/service/cliente-service.js
@@ -0,0 +1,51 @@
+const listaClientes = () =>  {
+    return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+        return resposta.json()
+    })
+
+    
+     // const promise = new Promise((resolve, reject) =>{
+     //     const http = new XMLHttpRequest();
+ 
+     //     http.open('GET','http://localhost:3000/profile');
+ 
+     //     http.onload = () =>{
+         
+     //        if(http.status>= 400){
+     //            reject(JSON.parse(http.response));
+     //        }
+     //        else{
+     //            resolve(JSON.parse(http.response)); 
+     //        }
+                 
+     //     }
+     //     http.send();
+     // })
+     // return promise;   
+}
+
+
+const criaCliente = () => {
+    return fetch (`http://localhost:3000/profile`, {
+        method : 'POST',
+        headers : {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome:nome ,
+            email:email
+        })
+
+      })
+    .then(resposta =>{
+        
+        return resposta.body
+    })
+}
+
+
+export const clienteService = { 
+    listaClientes,
+    criaCliente
+}
