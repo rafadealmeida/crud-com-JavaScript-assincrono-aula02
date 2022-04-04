f17e80036d43b7e3ce82f3ad5c471419bf237749
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-04-04 15:58:28 -0300

    Colocando async , com try, catch, e await

--- aula2/admin/controller/cadastraCliente--controller.js
+++ aula2/admin/controller/cadastraCliente--controller.js
@@ -4,18 +4,22 @@ const formulario = document.querySelector('[data-form]')
 
 console.log(formulario)
 
-formulario.addEventListener('submit', (evento) =>{
+formulario.addEventListener('submit', async (evento) =>{
 
     evento.preventDefault();
+    try{
 
-   const nome = evento.target.querySelector('[data-nome]').value;
-   const email = evento.target.querySelector('[data-email]').value;
-
-   
-
-   clienteService.criaCliente(nome,email)
-    .then(() =>{
+        const nome = evento.target.querySelector('[data-nome]').value;
+        const email = evento.target.querySelector('[data-email]').value;
+        
+        await clienteService.criaCliente(nome,email)
+        
         window.location.href = '../telas/cadastro_concluido.html'
-    })
+    }
+    catch(error) {
+        console.error(error)
+        window.location.href ='../telas/erro.html'
+    }
+    
 
 })
